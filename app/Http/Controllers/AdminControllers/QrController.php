f6a438423af8068414fdcd989db626ba447669b0
ARVIND_VERMA <arvindverma630635@gmail.com>
2024-11-28 13:17:47 +0530

    new

--- app/Http/Controllers/AdminControllers/QrController.php
+++ app/Http/Controllers/AdminControllers/QrController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
