e057ae9356499b58e70b8d0db331301304bbc805
arvindverma63 <arvindverma630635@gmail.com>
2025-04-27 21:17:13 +0530

    resolve route issue

--- app/Http/Controllers/AdminControllers/RestaurantBannerController.php
+++ app/Http/Controllers/AdminControllers/RestaurantBannerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admincontrollers;
+namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\RestaurantBanners;
